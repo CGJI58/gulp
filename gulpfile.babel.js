c7f0426abbb33ba086d8ef80e668ada13b058dcf
CGJI58 <leemsk22@naver.com>
2022-06-20 23:43:18 +0900

    #3.0에서 gulp-image 관련 기능들은 포기(주석처리하였음). 나머지는 이상없음

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,6 +2,7 @@ import gulp from "gulp";
 import gpug from "gulp-pug";
 import del from "del";
 import ws from "gulp-webserver";
+// import image from "gulp-image";
 
 const routes = {
   pug: {
@@ -9,6 +10,10 @@ const routes = {
     src: "src/*.pug",
     dest: "build",
   },
+  // img: {
+  //   src: "src/img/*",
+  //   dest: "build/img"
+  // }
 };
 
 const pug = () =>
@@ -19,14 +24,19 @@ const clean = () => del(["build/"]);
 const webserver = () =>
   gulp.src("build").pipe(ws({ livereload: true, open: true }));
 
+// const img = () =>
+//   gulp.src(routes.img.src).pipe(image()).pipe(gulp.dest(routes.img.dest));
+
 const watch = () => {
   gulp.watch(routes.pug.watch, pug);
+  // gulp.watch(routes.img.src, img);
 };
 
+// const prepare = gulp.series([clean, img]);
 const prepare = gulp.series([clean]);
 
 const assets = gulp.series([pug]);
 
-const postDev = gulp.series([webserver, watch]);
+const live = gulp.parallel([webserver, watch]);
 
-export const dev = gulp.series([prepare, assets, postDev]);
+export const dev = gulp.series([prepare, assets, live]);
