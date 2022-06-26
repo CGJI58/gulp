84f4fc8f047b4406019045fef0ce7291433bdfbe
CGJI58 <leemsk22@naver.com>
2022-06-26 17:10:20 +0900

    sass, minify, autoprefixer done

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,19 +2,29 @@ import gulp from "gulp";
 import gpug from "gulp-pug";
 import del from "del";
 import ws from "gulp-webserver";
-// import image from "gulp-image";
+import gimage from "gulp-image";
+import gulpSass from "gulp-sass";
+import nodeSass from "node-sass";
+import autoPrefixer from "gulp-autoprefixer";
+import miniCSS from "gulp-csso";
+
+const sass = gulpSass(nodeSass);
 
 const routes = {
   pug: {
-    // watch: "src/**/*.pug",
     watch: "src/*.pug",
     src: "src/*.pug",
     dest: "build",
   },
-  // img: {
-  //   src: "src/img/*",
-  //   dest: "build/img"
-  // }
+  img: {
+    src: "src/img/*",
+    dest: "build/img",
+  },
+  scss: {
+    watch: "src/scss/**/*.scss",
+    src: "src/scss/style.scss",
+    dest: "build/css",
+  },
 };
 
 const pug = () =>
@@ -25,18 +35,26 @@ const clean = () => del(["build/"]);
 const webserver = () =>
   gulp.src("build").pipe(ws({ livereload: true, open: true }));
 
-// const img = () =>
-//   gulp.src(routes.img.src).pipe(image()).pipe(gulp.dest(routes.img.dest));
+const img = () =>
+  gulp.src(routes.img.src).pipe(gimage()).pipe(gulp.dest(routes.img.dest));
+
+const styles = () =>
+  gulp
+    .src(routes.scss.src)
+    .pipe(sass().on("error", sass.logError))
+    .pipe(autoPrefixer())
+    .pipe(miniCSS())
+    .pipe(gulp.dest(routes.scss.dest));
 
 const watch = () => {
   gulp.watch(routes.pug.watch, pug);
-  // gulp.watch(routes.img.src, img);
+  gulp.watch(routes.img.src, img);
+  gulp.watch(routes.scss.watch, styles);
 };
 
-// const prepare = gulp.series([clean, img]);
-const prepare = gulp.series([clean]);
+const prepare = gulp.series([clean, img]);
 
-const assets = gulp.series([pug]);
+const assets = gulp.series([pug, styles]);
 
 const live = gulp.parallel([webserver, watch]);
 
